530713fc78cecdcf0f8ced97a5c7e74337ce75ed
Patricia <Patricia@Patricias-MacBook-Air.local>
2017-03-30 11:53:11 -0700

    Add more docstrings and comments

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 import emoji
 import sys
-import pdb
 import random
 
+#this global variable is necessary so a player cannot re-enter the same position twice within the same game
 already_seen_position = ''
 
 def who_goes_first():
@@ -16,15 +15,16 @@ def who_goes_first():
         return 'O'
 
 def play_again():
+    """ Asks the human if they would like to play another round of tic-tac-toe"""
     print(emoji.emojize("Would you like to play again? (Yes or no) :cherries: ",
                         use_aliases=True))
-    # pdb.set_trace()
     input = raw_input().lower()
     if input.startswith('y'):
         return True
     return False
 
 def get_move():
+    """Gets the human player's input, including some error-handling. """
     while True:
         move = raw_input("Please enter a move (from 1-9)> ")
         if move.isdigit() is False:
@@ -38,12 +38,12 @@ def get_move():
             return move
 
 def setup_board():
+  """Sets up an empty board. '_' is the equivalent to an empty space. """
 
   return [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
 
-
 def is_board_full(board):
-  """Return True is board is full, False otherwise.
+  """If board is full, return True. Otherwise, return False.
 
   >>> is_board_full([['_', '_', '_'], ['X', '_', 'O'], ['_', '_', '_']])
   False
@@ -61,32 +61,56 @@ def is_board_full(board):
 
   return True
 
+def computer_ai(board, player):
+  """
+  Computer algorithm:
+  - Check to see if a corner is open. If so, take it.
+  - Check to see if center is open. If so, take it.
+  - Take any of the middle-side spaces if open.
 
-def make_random_move(board, player):
-  """Find an empty cell and play into it.
+  Player is 'X' or 'O'
 
-  player = 'X' or 'O'
-
-  This should change the board in-place. It should return the
-  position (1-9) it played into.
+  Mutates the board in-place.
+  Returns position (1-9)
 
   >>> board = [['X', 'O', 'X'], ['X', 'X', 'O'], ['O', 'O', '_']]
-  >>> make_random_move(board, 'X')
+  >>> computer_ai(board, 'X')
   9
 
   >>> board
   [['X', 'O', 'X'], ['X', 'X', 'O'], ['O', 'O', 'X']]
   """
+ #checks top-left corner; if that space is free, take it
+  if board[0][0] == '_':
+      board[0][0] = player
+      return 1
+ #checks top-right corner; if that space is free, take it
+  elif board[0][2] == '_':
+      board[0][2] = player
+      return 3
+ #checks bottom-left corner; if that space is free, take it
+  elif board[2][0] == '_':
+      board[2][0] = player
+      return 7
+ #checks bottom-right corner; if that space is free, take it
+  elif board[2][2] == '_':
+      board[2][2] = player
+      return 9
+ #checks center; if that space is free, take it
+  elif board[1][1] == '_':
+      board[1][1] = player
+      return 5
+ #moves on one of the sides; if that space is free, take it
   for rowi in range(3):
-      for coli in range(3):
-          if board[rowi][coli] == '_':
-              board[rowi][coli] = player
-              board_placement = rowi * 3 + coli + 1
-              return board_placement
+    for coli in range(3):
+      if board[rowi][coli] == '_':
+          board[rowi][coli] = player
+          board_placement = rowi * 3 + coli + 1
+          return board_placement
 
-  raise Exception("No more empty spot!")
+  raise Exception("Nope, no more empty spaces.")
 
-def find_winner(bd):
+def find_winner(board):
   """"Given board, determine if winner. Return 'X', 'O', or None if no winner.
 
   >>> print find_winner([['_', '_', '_'], ['X', '_', 'O'], ['_', '_', '_']])
@@ -103,21 +127,21 @@ def find_winner(bd):
   """
   # Check for win in each row
   for rowi in range(3):
-      if bd[rowi][0] != '_' and bd[rowi][0] == bd[rowi][1] == bd[rowi][2]:
-          return bd[rowi][0]
+      if board[rowi][0] != '_' and board[rowi][0] == board[rowi][1] == board[rowi][2]:
+          return board[rowi][0]
 
   # Check for win in each col
   for coli in range(3):
-      if bd[0][coli] != '_' and bd[0][coli] == bd[1][coli] == bd[2][coli]:
-          return bd[0][coli]
+      if board[0][coli] != '_' and board[0][coli] == board[1][coli] == board[2][coli]:
+          return board[0][coli]
 
   # Check for \ diagonal
-  if bd[0][0] != '_' and bd[0][0] == bd[1][1] == bd[2][2]:
-      return bd[0][0]
+  if board[0][0] != '_' and board[0][0] == board[1][1] == board[2][2]:
+      return board[0][0]
 
   # Check for / diagonal
-  if bd[2][0] != '_' and bd[2][0] == bd[1][1] == bd[0][2]:
-      return bd[2][0]
+  if board[2][0] != '_' and board[2][0] == board[1][1] == board[0][2]:
+      return board[2][0]
 
 def print_board(board):
   """Given a board[col][row], print it.
@@ -158,6 +182,17 @@ def make_move(board, position, player):
   board[coli][rowi] = player
 
 def main_loop():
+  """This loop contains the overarching tic-tac-toe game logic.
+  
+  The human is Player X and the computer is Player O.
+  
+  Loop:
+  - Show the board
+  - Randomly choose whether the computer or human goes first
+  - If it's the human's turn, prompt for a position (1-9) and make their move
+  - If it's the computer's turn, the computer follows an algorithm to make any legal move
+  - If there's a winner and/or the board is full, exit this round """
+
   winner = None
   full = False
   global already_seen_position
@@ -172,13 +207,12 @@ def main_loop():
           print(emoji.emojize("It's your turn, please enter a digit :blue_heart:", use_aliases=True))
           move = get_move()
           already_seen_position += move
-        #   pdb.set_trace()
           position = int(move)
           make_move(board, position, 'X')
           current_player = 'O'
       else:
           print(emoji.emojize("It's the computer's turn, please wait...:purple_heart:", use_aliases=True))
-          position = make_random_move(board, 'O')
+          position = computer_ai(board, 'O')
           already_seen_position += str(position)
           print(emoji.emojize("The computer :computer: , Player :o: , played in position %s" % position, use_aliases=True))
           current_player = 'X'
@@ -188,23 +222,14 @@ def main_loop():
           print
           print_board(board)
           print
-          print "✨ Congratulations to %s ❗️" % winner
-        #   print(emoji.emojize(":tada:Congratulations to " + winner + ":sparkles: :raised_hands: ", use_aliases=True))
-      else:
-          print(emoji.emojize("~It's a tie~ :musical_note:", use_aliases=True))
+          print(emoji.emojize(":tada:Congratulations to Player %s:sparkles: :raised_hands: ", use_aliases=True)) % winner
+  if not winner and is_board_full(board):
+      print(emoji.emojize("There's no winner. It's a tie :musical_note:", use_aliases=True))
 
 def tic_tac_toe_game():
-      """Tic-tac-toe implementation--
-
-      The human will be X and the computer will be O.
-
-      Loop:
-      - Show the board
-      - If it's the human's turn, prompt for a position (1-9) and make their move
-      - If it's the computer's turn, make any legal move
-      - If there's a winner or the board is full, quit the game
-
-      Announce winner, if any.
+      """
+      - Ask the player if they would like to play again
+      - If the player would like to play again, this resets the global variable already_seen_position. Otherwise, the program exists.
       """
       while True:
           print(emoji.emojize("Welcome to a :snake: Pythonic :snake: tic-tac-toe game! \nLet's begin, shall we?", use_aliases=True))
@@ -216,7 +241,7 @@ def tic_tac_toe_game():
 
           if restart:
               global already_seen_position
-              already_seen_position = ''
+              already_seen_position = '' #resets seen positions global variable
           if not restart:
               print(emoji.emojize("Goodbye! Have a wonderful day :sunny:", use_aliases=True))
               sys.exit()
@@ -228,6 +253,6 @@ if __name__ == "__main__":
       import doctest
 
       if doctest.testmod().failed == 0:
-          print "\nTests passed! ⭐️ Hooray!\n"
+          print "\nTests passed! Hooray!\n"
   else:
     tic_tac_toe_game()
